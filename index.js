0d5b93300fcdda63700a06511813a5cdfdf55983
Coder-Akmal <97424640+Coder-Akmal@users.noreply.github.com>
2022-01-17 15:20:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+// for(let i=0; i<10; i++){
+//     console.log(i);
+// }
+// let k=0
+// do{
+//     console.log(k);
+//     k++
+// }while(k<10)
+// let j=0
+// while(0<10){ 
+//     console.log(j);
+//     j++
+// }
+// let javoblar=[]
+// let savollar=["qanday phone ishlatasiz?",
+// "yoshingiz nechida?",
+// "kasbingiz nima?"
+
+// ]
+// for(let i=0; i< savollar.length;i++){
+//     javoblar[i]=prompt(savollar[i])
+// }
+// console.log(javoblar);
+
+
+let i=[]
+let j=[prompt("Ismingiz nima?"),
+prompt("Familyangiz nima?"),
+prompt("Yoshingiz nima?")
+]
+while(i<1){
+    console.log(j);
+    i++
+}
+// do{
+//     i++
+//     console.log(j);
+// }while(i<j)
+
+
+
+
+
+
